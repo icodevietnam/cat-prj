da433f1aaa03ff94f245dc0f4cb55c1868d287ab
Louis Phan <icoding.active@gmail.com>
2016-04-07 18:36:45 +0700

    --- Cm ---

---
+++ app/Models/Answers.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\Models;
+
+use Core\Model;
+
+class Answers extends Model
+{
+	
+	function __construct()
+	{	
+		parent::__construct();
+	}
+}
---
+++ app/Models/Levels.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\Models;
+
+use Core\Model;
+
+class Levels extends Model
+{
+	
+	function __construct()
+	{	
+		parent::__construct();
+	}
+}
