e8440511a21a280761f66de33c71a7e180a5936c
Louis Phan <icoding.active@gmail.com>
2016-04-08 22:52:36 +0700

    Template ---

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -10,4 +10,12 @@ class Users extends Model
 	{	
 		parent::__construct();
 	}
+
+	//Get All
+
+	function getAll(){
+		$data = $this->_db->select("SELECT * FROM ".PREFIX."users WHERE username = :username", array(':username' => $username));
+		return $data[0]->password;
+	}
+
 }
